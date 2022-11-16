f529200b4cc7f8034732b52e5fbf2198824c816d
Dennis Meins <dennis.meins@adcubum.com>
2022-11-16 22:16:37 +0100

    F!! Added CommandLineRunner with static Logger and Todo-Comment

--- src/main/java/com/example/steinscheerepapier/SteinScheerePapierApplication.java
+++ src/main/java/com/example/steinscheerepapier/SteinScheerePapierApplication.java
@@ -1,13 +1,30 @@
 package com.example.steinscheerepapier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SteinScheerePapierApplication {
+public class SteinScheerePapierApplication implements CommandLineRunner {
+
+   private static final Logger LOG = LoggerFactory.getLogger(SteinScheerePapierApplication.class);
 
    public static void main(String[] args) {
       SpringApplication.run(SteinScheerePapierApplication.class, args);
    }
 
+   @Override
+   public void run(String... args) throws Exception {
+      /*
+       * Stein-Scheere-Papier Challenge
+       * - Zwei Computerspieler A und B spielen 100 mal gegeneinander
+       * - Spieler A spielt zufällig eines der Symbole Stein, Scheere oder Papier
+       * - Spieler B spielt konstant mit dem Symbol Papier
+       * - Beide Spieler werden vom Programm ohne Nutzereingabe "gespielt"
+       * - Das Ergebnis wird am Ende aller Runden auf der Console ausgegeben
+       */
+      LOG.info("Running game 'stone scissor paper'");
+   }
 }
