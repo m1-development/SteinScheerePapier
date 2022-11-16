5bb8351adc9643d16f38b0d092151d425957feb0
Dennis Meins <dennis.meins@adcubum.com>
2022-11-16 23:38:19 +0100

    F!! Extended PLayer class with method for getting random symbol of 'stone, scissor or paper'

--- src/main/java/com/example/steinscheerepapier/gamer/Player.java
+++ src/main/java/com/example/steinscheerepapier/gamer/Player.java
@@ -1,10 +1,24 @@
 package com.example.steinscheerepapier.gamer;
 
+import java.util.Random;
+
 import com.example.steinscheerepapier.common.Symbol;
 
 public class Player {
 
    Symbol getConstantPaperSymbol() {
-      return Symbol.Paper;
+      return Symbol.PAPER;
    }
+
+   public int getRandomIntNumber(int min, int max) {
+      return new Random().ints(min, max)
+            .findFirst()
+            .orElseThrow();
+   }
+
+   Symbol getRandomSymbol() {
+      int randomIntNumber = getRandomIntNumber(0, 2);
+      return Symbol.values()[randomIntNumber];
+   }
+
 }
--- src/test/java/com/example/steinscheerepapier/gamer/PlayerTest.java
+++ src/test/java/com/example/steinscheerepapier/gamer/PlayerTest.java
@@ -1,8 +1,18 @@
 package com.example.steinscheerepapier.gamer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.example.steinscheerepapier.common.Symbol;
 
@@ -17,6 +27,39 @@ class PlayerTest {
       Symbol symbol = player.getConstantPaperSymbol();
 
       // then
-      assertEquals(Symbol.Paper, symbol);
+      assertEquals(Symbol.PAPER, symbol);
+   }
+
+   @ParameterizedTest
+   @MethodSource("getValuePairs")
+   void getRandomSymbolShouldReturnCorrectSymbolForIntValue(int value, Symbol expectedSymbol) {
+      Player player = spy(new Player());
+      doReturn(value).when(player).getRandomIntNumber(anyInt(), anyInt());
+
+      // when
+      Symbol symbol = player.getRandomSymbol();
+
+      // then
+      assertEquals(expectedSymbol, symbol);
+   }
+
+   private static Stream<Arguments> getValuePairs() {
+      return Stream.of(
+            arguments(0, Symbol.STONE),
+            arguments(1, Symbol.SCISSOR),
+            arguments(2, Symbol.PAPER)
+      );
+   }
+
+   @Test()
+   void getRandomIntNumberShouldThrowExceptionForValue3() {
+      // given
+      Player player = spy(new Player());
+      doReturn(3).when(player).getRandomIntNumber(anyInt(), anyInt());
+
+      // when
+      assertThrows(ArrayIndexOutOfBoundsException.class, () -> player.getRandomSymbol());
+
+      // then
    }
 }
