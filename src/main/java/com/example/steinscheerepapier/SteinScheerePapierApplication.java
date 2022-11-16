85be0c70bf5f709a7b64b592451ad7317ca544e8
Dennis Meins <dennis.meins@adcubum.com>
2022-11-16 21:44:43 +0100

    F!! Initial Spring Boot Application based on Maven

---
+++ src/main/java/com/example/steinscheerepapier/SteinScheerePapierApplication.java
@@ -0,0 +1,13 @@
+package com.example.steinscheerepapier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteinScheerePapierApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SteinScheerePapierApplication.class, args);
+   }
+
+}
